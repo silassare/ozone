203f8ad3bc778b0a182a89bb189cc30aba7a0a32
Emile Silas SARE <emile.silas@gmail.com>
2019-08-28 16:29:31 +0100

    reformat

--- oz/oz_settings/oz.files.php
+++ oz/oz_settings/oz.files.php
@@ -20,14 +20,17 @@
 		// 		/ozone-7000000000-fe5017db3a4b07eb5297c745ba198355-1.o
 		// 		/ozone-7000000000-fe5017db3a4b07eb5297c745ba198355-1
 		'OZ_GET_FILE_URI_EXTRA_FORMAT' => 'ozone-{oz_file_id}-{oz_file_key}-{oz_file_quality}[.{oz_file_extension}]',
+
 		// the name used, when user download a file
 		// you can use oz_file_id, oz_file_quality and oz_file_extension
 		'OZ_GET_FILE_NAME'             => 'ozone-{oz_file_id}-{oz_file_quality}.{oz_file_extension}',
+
 		// who is able to access files through the "files service"
 		// 	users				=> serve file only for verified users
 		// 	session (default)	=> serve file only when there is an active session
 		// 	any					=> serve file without restriction
 		'OZ_GET_FILE_ACCESS_LEVEL'     => 'session',
+
 		'OZ_CAPTCHA_FILE_NAME'         => 'oz-captcha-{oz_captcha_key}.png',
 		'OZ_CAPTCHA_URI_EXTRA_FORMAT'  => 'oz-captcha-{oz_captcha_key}.png',
 		'OZ_QR_CODE_FILE_NAME'         => 'oz-qr-code-{oz_qr_code_key}.png',
