30f7eaacf8631d8a775b2156a3e1b26f53bb5deb
Emile Silas SARE <emile.silas@gmail.com>
2021-01-13 16:35:16 +0100

    OZ_SELF_SECURITY_CHECK is deprectaed

--- oz/index.php
+++ oz/index.php
@@ -1,33 +1,36 @@
-#!/usr/bin/env php
 <?php
 
-	/**
-	 * Copyright (c) 2017-present, Emile Silas Sare
-	 *
-	 * This file is part of OZone (O'Zone) package.
-	 *
-	 * For the full copyright and license information, please view the LICENSE
-	 * file that was distributed with this source code.
-	 */
+/**
+ * Copyright (c) 2017-present, Emile Silas Sare
+ *
+ * This file is part of OZone (O'Zone) package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
+use OZONE\OZ\Cli\Cli;
 
+// For backward compatibility only
+// @deprecated 2.0.0
+\define('OZ_SELF_SECURITY_CHECK', 1);
 
-	// Don't forget to use DS instead of \ or / and always add the last DS to your directories path
-	\define('DS', \DIRECTORY_SEPARATOR);
+// Don't forget to use DS instead of \ or / and always add the last DS to your directories path
+\define('DS', \DIRECTORY_SEPARATOR);
 
-	// Project directory
-	\define('OZ_PROJECT_DIR', \getcwd() . DS);
+// Project directory
+\define('OZ_PROJECT_DIR', \getcwd() . DS);
 
-	// OZone directory
-	\define('OZ_OZONE_DIR', __DIR__ . DS);
+// OZone directory
+\define('OZ_OZONE_DIR', __DIR__ . DS);
 
-	// OZone app directory
-	\define('OZ_APP_DIR', OZ_PROJECT_DIR . 'api' . DS . 'app' . DS);
+// OZone app directory
+\define('OZ_APP_DIR', OZ_PROJECT_DIR . 'api' . DS . 'app' . DS);
 
-	// Logs directory
-	\define('OZ_LOG_DIR', OZ_PROJECT_DIR);
+// Logs directory
+\define('OZ_LOG_DIR', OZ_PROJECT_DIR);
 
-	include_once OZ_OZONE_DIR . 'Cli' . DS . 'Cli.php';
+include_once OZ_OZONE_DIR . 'Cli' . DS . 'Cli.php';
 
-	$oz_cli = new \OZONE\OZ\Cli\Cli();
-	$oz_cli->run($argv);
+$oz_cli = new Cli();
+$oz_cli->run($argv);
