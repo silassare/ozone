21114903eab4f22f3b8eeae2f66983073bb3befd
Emile Silas SARE <emile.silas@gmail.com>
2021-01-17 16:40:14 +0100

    O'Zone not OZone

--- oz/oz_default/oz_define.php
+++ oz/oz_default/oz_define.php
@@ -8,10 +8,10 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-	
-	\define('OZ_OZONE_VERSION', '2.0.0');
-	\define('OZ_OZONE_VERSION_NAME', 'OZone v' . OZ_OZONE_VERSION);
-	\define('OZ_OZONE_IS_CLI', 'cli' === \php_sapi_name());
+
+\define('OZ_OZONE_VERSION', '2.0.0');
+\define('OZ_OZONE_VERSION_NAME', 'O\'Zone v' . OZ_OZONE_VERSION);
+\define('OZ_OZONE_IS_CLI', 'cli' === \php_sapi_name());
 
 if (!\defined('PHP_INT_MIN')) {
 	// Available since PHP 7.0.0 http://php.net/manual/en/reserved.constants.php
