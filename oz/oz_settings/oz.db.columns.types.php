15d1a7f667f933bb3015c9995a8183b6166a3758
Emile Silas SARE <emile.silas@gmail.com>
2021-02-09 16:55:37 +0100

    type url added

--- oz/oz_settings/oz.db.columns.types.php
+++ oz/oz_settings/oz.db.columns.types.php
@@ -8,16 +8,17 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-	
-	return [
-		'phone'     => '\OZONE\OZ\Columns\Types\TypePhone',
-		'email'     => '\OZONE\OZ\Columns\Types\TypeEmail',
-		'date'      => '\OZONE\OZ\Columns\Types\TypeDate',
-		'user_name' => '\OZONE\OZ\Columns\Types\TypeUserName',
-		'password'  => '\OZONE\OZ\Columns\Types\TypePassword',
-		'cc2'       => '\OZONE\OZ\Columns\Types\TypeCC2',
-		'gender'    => '\OZONE\OZ\Columns\Types\TypeGender',
-		'timestamp' => '\OZONE\OZ\Columns\Types\TypeTimestamp',
-		'file'      => '\OZONE\OZ\Columns\Types\TypeFile',
-		// 'image' => '\OZONE\OZ\Columns\Types\TypeImage'
-	];
+
+return [
+	'phone'     => '\OZONE\OZ\Columns\Types\TypePhone',
+	'email'     => '\OZONE\OZ\Columns\Types\TypeEmail',
+	'url'       => '\OZONE\OZ\Columns\Types\TypeUrl',
+	'date'      => '\OZONE\OZ\Columns\Types\TypeDate',
+	'user_name' => '\OZONE\OZ\Columns\Types\TypeUserName',
+	'password'  => '\OZONE\OZ\Columns\Types\TypePassword',
+	'cc2'       => '\OZONE\OZ\Columns\Types\TypeCC2',
+	'gender'    => '\OZONE\OZ\Columns\Types\TypeGender',
+	'timestamp' => '\OZONE\OZ\Columns\Types\TypeTimestamp',
+	'file'      => '\OZONE\OZ\Columns\Types\TypeFile',
+	// 'image' => '\OZONE\OZ\Columns\Types\TypeImage'
+];
