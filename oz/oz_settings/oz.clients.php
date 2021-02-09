836b1e10c90e93294eb1713de049646510998483
Emile Silas SARE <emile.silas@gmail.com>
2021-02-09 16:56:18 +0100

    OZ_CORS_ALLOWED_HEADERS added

--- oz/oz_settings/oz.clients.php
+++ oz/oz_settings/oz.clients.php
@@ -8,21 +8,23 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-	
-	return [
-		/*
-		 * how can we get stuff from OZone if set to:
-		 *
-		 * - any
-		 *     - allow CORS for any host
-		 *
-		 * - check
-		 *     - the host of the request header 'Origin' must be the same as the host of the api key client url
-		 *
-		 * - deny
-		 *    - disable CORS
-		 *
-		 * - default: check (even if you mistype the value)
-		 */
-		'OZ_CORS_ALLOW_RULE' => 'check',
-	];
+
+return [
+	/*
+		* how can we get stuff from OZone if set to:
+		*
+		* - any
+		*     - allow CORS for any host
+		*
+		* - check
+		*     - the host of the request header 'Origin' must be the same as the host of the api key client url
+		*
+		* - deny
+		*    - disable CORS
+		*
+		* - default: check (even if you mistype the value)
+		*/
+	'OZ_CORS_ALLOW_RULE' => 'check',
+
+	'OZ_CORS_ALLOWED_HEADERS' => ['accept', 'content-type'],
+];
