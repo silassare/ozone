69b2c7a244207e9a73f1fc553fbb1802b60660bd
Emile Silas SARE <emile.silas@gmail.com>
2021-02-09 16:57:15 +0100

    comment added

--- oz/oz_settings/oz.sessions.php
+++ oz/oz_settings/oz.sessions.php
@@ -8,8 +8,10 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-	
-	return [
-		'OZ_SESSION_TOKEN_HEADER_NAME'    => 'x-ozone-session-token',
-		'OZ_SESSION_TOKEN_HEADER_ENABLED' => false,
-	];
+
+return [
+	// Used to share same session
+	// Make sure you know what you're doing.
+	'OZ_SESSION_TOKEN_HEADER_NAME'    => 'x-ozone-session-token',
+	'OZ_SESSION_TOKEN_HEADER_ENABLED' => false,
+];
