249c56b4ff278fb5b8b31062eded09a90b29b478
Emile Silas SARE <emile.silas@gmail.com>
2021-04-14 14:27:26 +0100

    added

---
+++ oz/oz_default/oz_bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Copyright (c) 2017-present, Emile Silas Sare
+ *
+ * This file is part of OZone package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+include_once __DIR__ . \DIRECTORY_SEPARATOR . 'oz_config.php';
+
+include_once __DIR__ . \DIRECTORY_SEPARATOR . 'oz_define.php';
+
+include_once __DIR__ . \DIRECTORY_SEPARATOR . 'oz_func.php';
