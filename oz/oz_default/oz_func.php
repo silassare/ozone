a0d2cdfa444b815f365db60e6b164dcfd32ecd11
Emile Silas SARE <emile.silas@gmail.com>
2023-11-16 01:51:31 +0100

    :sparkles: `oz_trace` helper added

--- oz/oz_default/oz_func.php
+++ oz/oz_default/oz_func.php
@@ -14,9 +14,9 @@ declare(strict_types=1);
 use Gobl\DBAL\Interfaces\RDBMSInterface;
 use OZONE\Core\App\Db;
 use OZONE\Core\App\Interfaces\AppInterface;
+use OZONE\Core\Exceptions\RuntimeException;
 use OZONE\Core\Logger\Logger;
 use OZONE\Core\OZone;
-use PHPUtils\Env\EnvParser;
 
 if (!\function_exists('oz_logger')) {
 	/**
@@ -27,6 +27,44 @@ if (!\function_exists('oz_logger')) {
 	function oz_logger(mixed $value): void
 	{
 		Logger::log($value);
+	}
+
+	/**
+	 * Write a stack trace to log file.
+	 *
+	 * This is useful for debugging and prevent possible throwable silencing
+	 * that may occur in upper catch blocks and lead to unexpected behavior.
+	 *
+	 * This is one of the most common mistakes:
+	 *
+	 * ```
+	 * try {
+	 *    functionWithImportantLogicThatMayThrow();
+	 * } catch (Throwable) {
+	 *   // the error is silenced here
+	 * }
+	 *
+	 * // to solve this problem, you can use oz_trace like this:
+	 * function functionWithImportantLogicThatMayThrow(): void {
+	 *   try {
+	 *    // some code
+	 *  } catch (Throwable $t) {
+	 *   oz_trace('functionWithImportantLogicThatMayThrow failed', ['data' => 'some data'], $t);
+	 *   throw $t; // rethrow the error that can be silenced in upper catch blocks
+	 * }
+	 *```
+	 *
+	 * @param string         $message  The error message
+	 * @param null|array     $data     The error data
+	 * @param null|Throwable $previous The previous exception
+	 */
+	function oz_trace(string $message, ?array $data = [], ?Throwable $previous = null): void
+	{
+		try {
+			throw new RuntimeException($message, $data, $previous);
+		} catch (RuntimeException $e) {
+			oz_logger($e);
+		}
 	}
 
 	/**
