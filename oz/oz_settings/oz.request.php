5d91d987beb9abaf5bc5fd1a2a9a539413aa6ecc
Emile Silas SARE <emile.silas@gmail.com>
2025-09-30 13:06:26 +0100

    :zap: CORS configuration documentation added

--- oz/oz_settings/oz.request.php
+++ oz/oz_settings/oz.request.php
@@ -12,19 +12,49 @@
 declare(strict_types=1);
 
 return [
+	/**
+	 * Allowed CORS headers.
+	 */
 	'OZ_CORS_ALLOWED_HEADERS'     => ['accept', 'content-type'],
 
-	// allowed origin:
-	// - '*' for any
-	// - 'http://example.com' for a specific host
-	// - 'self' for the request url host
+	/**
+	 * Allowed CORS origin.
+	 *
+	 * - 'self' for the request url host
+	 * - 'http://example.com' for a specific host
+	 * - '*' for any
+	 *
+	 * @default '*'
+	 */
 	'OZ_CORS_ALLOWED_ORIGIN'      => '*',
-	// this is a integer value in seconds used for
-	// Access-Control-Max-Age header
-	// default is 86400 seconds (24 hours)
+
+	/**
+	 * Access-Control-Max-Age header value.
+	 *
+	 * @default 86400 (24 hours)
+	 */
 	'OZ_CORS_ALLOWED_MAX_AGE'      => 86400,
+
+	/**
+	 * Default origin to use when the request does not have an origin header.
+	 *
+	 * @default 'http://localhost'
+	 */
 	'OZ_DEFAULT_ORIGIN'            => 'http://localhost',
-	// = For server that does not support HEAD, PATCH, PUT, DELETE...,
+
+	/**
+	 * Allow to use X-OZONE-Real-Method (like X-HTTP-Method-Override) header to simulate other HTTP methods.
+	 *
+	 * For server that does not support HEAD, PATCH, PUT, DELETE...,
+	 *
+	 * @example: use X-OZONE-Real-Method: DELETE to simulate a DELETE request while sending a POST request.
+	 */
 	'OZ_ALLOW_REAL_METHOD_HEADER' => true,
-	'OZ_REAL_METHOD_HEADER_NAME'  => 'x-ozone-real-method',
+
+	/**
+	 * Name of the header to use to override HTTP method.
+	 *
+	 * @default 'X-OZONE-Real-Method'
+	 */
+	'OZ_REAL_METHOD_HEADER_NAME'  => 'X-OZONE-Real-Method',
 ];
