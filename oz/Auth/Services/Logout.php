d7756cfd4969a60c3ef03ce1f6abce4b65779fcc
Emile Silas SARE <emile.silas@gmail.com>
2025-09-30 13:06:02 +0100

    :zap: add Clear-Site-Data header

--- oz/Auth/Services/Logout.php
+++ oz/Auth/Services/Logout.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 namespace OZONE\Core\Auth\Services;
 
 use OZONE\Core\App\Service;
+use OZONE\Core\App\Settings;
 use OZONE\Core\Router\RouteInfo;
 use OZONE\Core\Router\Router;
 
@@ -44,7 +45,15 @@ final class Logout extends Service
 				$s = new self($ri);
 				$s->actionLogout();
 
-				return $s->respond();
+				$response = $s->respond();
+
+				if (Settings::get('oz.cache', 'OZ_CLEAR_SITE_DATA_HEADER_ON_LOGOUT')) {
+					$value = Settings::get('oz.cache', 'OZ_CLEAR_SITE_DATA_HEADER_VALUE');
+
+					return $response->withHeader('Clear-Site-Data', $value);
+				}
+
+				return $response;
 			})
 			->name(self::ROUTE_LOGOUT);
 	}
--- oz/oz_settings/oz.cache.php
+++ oz/oz_settings/oz.cache.php
@@ -15,8 +15,29 @@ use OZONE\Core\Cache\Drivers\DbCache;
 use OZONE\Core\Cache\Drivers\RuntimeCache;
 
 return [
-	// Cache Driver that destroy data after runtime
+	/**
+	 * Runtime cache driver: data are lost after runtime.
+	 */
 	'OZ_RUNTIME_CACHE_PROVIDER'    => RuntimeCache::class,
-	// Shared cache driver system: Db, Redis, Memcached...
+
+	/**
+	 * Persistent cache driver: data should survive after runtime.
+	 */
 	'OZ_PERSISTENT_CACHE_PROVIDER' => DbCache::class,
+
+	/**
+	 * Add Clear-Site-Data header on logout response.
+	 *
+	 * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Clear-Site-Data
+	 *
+	 * @default false
+	 */
+	'OZ_CLEAR_SITE_DATA_HEADER_ON_LOGOUT' => true,
+
+	/**
+	 * Value of Clear-Site-Data header on logout response.
+	 *
+	 * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Clear-Site-Data
+	 */
+	'OZ_CLEAR_SITE_DATA_HEADER_VALUE' => '"cache", "cookies", "storage", "executionContexts"',
 ];
